3d3c0db68b4f94e5285520573cd80b9544c3d356
Maven Inventing <marison@maven.com.br>
2026-06-25 14:21:59 -0300

    feat: adiciona modelo de posicionamento da assinatura

---
+++ src/main/java/br/com/privacytools/assinador/signing/SignaturePlacement.java
@@ -0,0 +1,10 @@
+package br.com.privacytools.assinador.signing;
+
+public record SignaturePlacement(
+        int pageIndex,
+        float x,
+        float y,
+        float width,
+        float height
+) {
+}
